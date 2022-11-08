944de7e709d2039f8dc07e4a9ffcc9a5a424cfb0
posdno <molodkina.dasha@mail.ru>
2022-11-08 19:05:29 +0300

    Добавили дз1

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,23 +1,22 @@
-﻿/// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее 
+﻿// Напишите программу, которая на вход принимает 2 числа и выдает какое число максимальное, а какое минимальное 
 
 int a = new Random().Next(1,  10);
 Console.WriteLine(a);
 int b = new Random().Next(1,  10);
 Console.WriteLine(b);
-
 int max = a;
 int min = a;
 
 if (b > max)
 {
-  max = b;
+    max = b;
 }
-else 
+else
 {
-  min = b;
+    min = b;
 }
 
-Console.Write("max    ");
+Console.Write("max   ");
 Console.WriteLine(max);
-Console.Write("min    ");
+Console.Write("min   ");
 Console.WriteLine(min);
