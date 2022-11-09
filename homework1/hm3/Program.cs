164ec67b08e20078e20ddb1facdebf0682a75036
posdno <molodkina.dasha@mail.ru>
2022-11-09 18:25:51 +0300

    Изменение структуры

---
+++ homework1/hm3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу,которая на вход принимает число и выдает, является ли число четным
+
+int a = new Random().Next(1,    10);
+if (a%2 == 0)
+{
+    Console.Write(a);
+    Console.WriteLine("  -  четное число");
+}
+else
+{
+    Console.Write(a);
+    Console.WriteLine("  -  нечетное число");
+}
+
---
+++ homework1/hw1/hw4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+
+int N = new Random().Next(1,  111);
+Console.Write("Интервал от 1 до  ");
+Console.WriteLine(N);
+Console.Write("Четные числа:  ");
+int a = 1;
+while (a<=N)
+{
+    if (a % 2 ==0)
+    {
+        Console.WriteLine(a);
+    }
+    a = a + 1;
+}
