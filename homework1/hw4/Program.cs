c3a4f9189bea20d1c35fbaa159dedb65d9449ff9
posdno <molodkina.dasha@mail.ru>
2022-11-09 19:04:09 +0300

    Добавили дз

---
+++ homework1/hw4/Program.cs
@@ -0,0 +1,13 @@
+﻿int N = new Random().Next(1,  10);
+Console.Write("Выбранный интервал от 1 до ");
+Console.WriteLine(N);
+Console.Write("Четные числа: ");
+int a = 1;
+while (a<=N)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+    }
+    a=a+1;
+}
