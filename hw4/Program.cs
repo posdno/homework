a51f54c1c77b85ca13d8b189bfbc6d7567ebe470
posdno <molodkina.dasha@mail.ru>
2022-11-10 11:49:11 +0300

    This is a new commit for what I originally planned to be amended

--- hw4/Program.cs
+++ hw4/Program.cs
@@ -1,15 +1,13 @@
-﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
-
-int N = new Random().Next(1,  111);
-Console.Write("Интервал от 1 до  ");
+﻿int N = new Random().Next(1,  10);
+Console.Write("Выбранный интервал от 1 до ");
 Console.WriteLine(N);
-Console.Write("Четные числа:  ");
+Console.Write("Четные числа: ");
 int a = 1;
 while (a<=N)
 {
-    if (a % 2 ==0)
+    if (a % 2 == 0)
     {
         Console.WriteLine(a);
     }
-    a = a + 1;
+    a=a+1;
 }
